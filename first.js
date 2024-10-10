d8fdb5a24895fe98030d6f72eca5bd4ec2d22b87
Ajay-singh-github <ajaysikata@gmail.com>
2024-10-10 19:17:03 +0530

    added cv button

--- first.js
+++ first.js
@@ -5,19 +5,24 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.main-container');
     const li=document.querySelectorAll(".nav-list li a");
-    const navl = document.querySelector(".nav-list li a::after,.nav-list .active::after")
+    const navl = document.querySelector(".nav-list li a::after,.nav-list .active::after");
+    const arrowa = document.querySelector(".up-arrow-container");
+   
     if (window.scrollY > 500) {
       // If scrolled more than 50px, change background to blue
+      arrowa.style.display='flex'
       header.style.backgroundColor = 'white';
       li[0].style.color ="#0078ff";
       li[1].style.color ="#0078ff";
       li[2].style.color ="#0078ff";
       li[3].style.color ="#0078ff";
-      navl.style.color = "#0078ff"
+      navl.style.color = "#0078ff";
+      
       
       //transition: transform .5s;
     } else {
       // If scrolled back to the top, revert to original color
+      arrowa.style.display='none'
       header.style.backgroundColor = "transparent";
       li[0].style.color = 'white';
       li[1].style.color = 'white';
